5c369ee74780c79488d1bdf80912830cd4f16665
seungheonL <44883967+seungheonL@users.noreply.github.com>
2021-12-14 00:18:53 +0900

    Create JadenCase_문자열 만들기.cpp

---
+++ Level 2/JadenCase_문자열 만들기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    for(int i=0; i<s.size(); i++)
+    {
+        if(i == 0)
+        {
+            s[i] = toupper(s[i]);
+        }
+        else if(s[i-1] == ' ')
+        {
+            s[i] = toupper(s[i]);
+        }
+        else
+        {
+            s[i] = tolower(s[i]);
+        }
+    }
+    
+    return s;
+}
