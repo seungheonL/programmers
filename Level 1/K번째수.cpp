ea03326ca627abbd308e8ad0802b9f613e73b487
seungheonL <44883967+seungheonL@users.noreply.github.com>
2022-01-03 15:47:58 +0900

    Create K번째수.cpp

---
+++ Level 1/K번째수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(int i=0; i<commands.size(); i++)
+    {
+        vector<int> v;
+        
+        for(int j=commands[i][0]; j<=commands[i][1]; j++)
+        {
+            v.push_back(array[j-1]);
+        }
+        
+        sort(v.begin(), v.end());
+        
+        answer.push_back(v[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
