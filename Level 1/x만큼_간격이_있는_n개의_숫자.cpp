7407517ee18146f6e2a52db50ead4399de1a0b5d
seungheonL <44883967+seungheonL@users.noreply.github.com>
2022-01-12 21:18:38 +0900

    Create x만큼_간격이_있는_n개의_숫자.cpp

---
+++ Level 1/x만큼_간격이_있는_n개의_숫자.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    int num = x;
+    while(n--)
+    {
+        answer.push_back(num);
+        num += x;
+    }
+    
+    return answer;
+}
